7b2f1dc0f8c6e47d6575f19ad4fe8dc317f4f85e
samim5274 <banglarcele1122@gmail.com>
2026-04-15 15:48:48 +0600

    Image showing problem fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('path.public', function() {
+            return base_path('public');
+        });
     }
 
     /**
